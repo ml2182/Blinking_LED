220776b89b4164215eabe67e41381e4e0c7a4ac0
ml2182 <ml2182@cam.ac.uk>
2025-12-13 16:02:30 +0000

    Finalish version Blinking LED - updating gitignore

--- scripts/create_project.tcl
+++ scripts/create_project.tcl
@@ -7,16 +7,5 @@ create_project BlinkingLED ./BlinkingLED -part xc7a35tcpg236-1 -force
 add_files ./src/Blinking_LED.sv
 add_files ./src/tb_Blinking_LED.sv
 
-# Add your constraints
-#add_files ./constraints/blinking_led.xdc
-
-# Import IP cores (if you have any)
-#import_ip ./ip/my_core.xci
-
-# Recreate block design if you exported one
-#source ./bd/my_design_bd.tcl
 set_property top tb_Blinking_LED [get_filesets sim_1]
-# Run synthesis and implementation
 launch_simulation
-#launch_runs synth_1 when i get the FPGA
-#launch_runs impl_1
